9dc50b1f005658df9e8f54b814111e9650d007cf
Sumanaswi <sumanaswimedapati@gmail.com>
2026-03-29 16:24:59 +0530

    Remove vercel.json and vercel configs

--- rfq_auction/rfq_auction/settings.py
+++ rfq_auction/rfq_auction/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-@58bt-1&u$n!9l6gr0($(huo(mm_iq=7au8%i7%kn01!w+40d2')
+SECRET_KEY = 'django-insecure-@58bt-1&u$n!9l6gr0($(huo(mm_iq=7au8%i7%kn01!w+40d2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'localhost:8000', '127.0.0.1:8000', '.vercel.app']
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', 'localhost:8000', '127.0.0.1:8000']
 
 
 # Application definition
@@ -132,8 +131,7 @@ TIME_ZONE = 'Asia/Kolkata'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
